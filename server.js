4ad204545c5437d6338a74ec452a25504e635ce7
tam <taam.tr17@gmail.com>
2018-12-13 14:10:12 -0800

    add PORT for server

---
+++ server.js
@@ -0,0 +1,24 @@
+var express = require("express");
+var path = require("path");
+var bodyParser = require("body-parser");
+
+//Tells node that we are creating an "express" server 
+var app = express();
+
+var PORT = process.env.PORT || 3000;
+
+// Set up the Express app to handle data parsing
+// app.use(express.urlencoded({extended: true}));
+// app.use(express.json());
+
+// ROUTER
+// The routes give our server a "map" of how to respond when users visit or request data from various URLs
+
+// require("./app/routing/apiRoutes")(app);
+// require("./app/routing/htmlRoutes")(app);
+
+// LISTENER
+app.listen(PORT, function(){
+    console.log("App listening on PORT: " + PORT);
+    
+})
