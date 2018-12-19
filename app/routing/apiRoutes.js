de9fc6bd72902f60d68b14703a167e190d455dfe
tam <taam.tr17@gmail.com>
2018-12-18 20:38:22 -0800

    Styling pages

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -11,31 +11,18 @@ module.exports = function (app) {
     })
 
     // User submits a survey form and it submits data to the server
-    app.post("/api/friends", function (req, res) {
-        console.log("TESTING POST");
-        
-        // With that done, compare the difference between current user's scores against those from other users, question by question. Add up the differences to calculate the totalDifference
-        // Compare those results against every user in the database.
-        // 
-        var bestMatch = {
-            name: "",
-            photo: "",
-            matchDifference: 1000
-        };
 
-        // The results of the user's survey POST and parse it
-        var newFriends = req.body;
-        var userScores = newFriends.scores;
-        
-        var totalDifference = 0;
+    app.post("api/friends", function(req, res){
+        var newFriend = req.body;
+        var scores = newFriend.score;
 
-        // Here we loop through all the friend possibilities in the database.
-        for (var i = 0; i < friends.length; i++){
-            console.log(friends[i].name);
-            totalDifference = 0;
-            
+        newFriend.routeName = newFriend.name.replace(/\s+/g, "").toLowerCase();
+
+        console.log(newFriend);
         
-           
-        }
+        friends.push(newFriend);
+        res.json(newFriend);
+        
+
     })
 }
